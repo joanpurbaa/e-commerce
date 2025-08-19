781784f77d3b3af19faacf68c126065f31030579
joan purba <joanpurba562@gmail.com>
2025-08-19 21:05:36 +0700

    feat: create new card page success pop up and fix the input

--- src/app/new-card/new-card.page.ts
+++ src/app/new-card/new-card.page.ts
@@ -1,16 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-new-card',
   templateUrl: './new-card.page.html',
   styleUrls: ['./new-card.page.scss'],
-  standalone: false
+  standalone: false,
 })
 export class NewCardPage implements OnInit {
+  newCardForm!: FormGroup;
+  cardNumber: string = '';
+  expiryDate: string = '';
+  securityCode: string = '';
+  successPopupStatus: boolean = false;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit() {
+    this.newCardForm = this.formBuilder.group({
+      cardNumber: ['', [Validators.required]],
+      expiryDate: ['', [Validators.required]],
+      securityCode: ['', [Validators.required]],
+    });
   }
 
+  onSubmit() {
+    if (this.newCardForm.valid) {
+      console.log(this.newCardForm.value);
+    } else {
+      console.log(this.newCardForm.value);
+      console.log('data form tidak valid');
+    }
+  }
+
+  setSuccessPopupStatus() {
+    this.successPopupStatus = !this.successPopupStatus;
+  }
 }
