d04ac9b47a92db66a430d083eb912781434723da
joan purba <joanpurba562@gmail.com>
2025-08-21 13:59:37 +0700

    feat: create cart feature

--- src/app/cart/cart.page.ts
+++ src/app/cart/cart.page.ts
@@ -7,7 +7,46 @@ import { Component, OnInit } from '@angular/core';
   standalone: false,
 })
 export class CartPage implements OnInit {
-  constructor() {}
+  cart: any[] = [];
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.loadCart();
+  }
+
+  loadCart() {
+    this.cart = JSON.parse(localStorage.getItem('cart') || '[]');
+  }
+
+  saveCart() {
+    localStorage.setItem('cart', JSON.stringify(this.cart));
+  }
+
+  removeFromCart(id: number) {
+    this.cart = this.cart.filter((item) => item.id !== id);
+    this.saveCart();
+  }
+
+  increaseQty(id: number) {
+    let item = this.cart.find((p) => p.id === id);
+    if (item) {
+      item.quantity += 1;
+      this.saveCart();
+    }
+  }
+
+  decreaseQty(id: number) {
+    let item = this.cart.find((p) => p.id === id);
+    if (item) {
+      if (item.quantity > 1) {
+        item.quantity -= 1;
+      } else {
+        this.removeFromCart(id);
+      }
+      this.saveCart();
+    }
+  }
+
+  getSubTotal() {
+    return this.cart.reduce((acc, item) => acc + item.price * item.quantity, 0);
+  }
 }
--- src/app/details/details.page.ts
+++ src/app/details/details.page.ts
@@ -21,4 +21,24 @@ export class DetailsPage {
         this.product = data;
       });
   }
+
+  addToCart(product: any) {
+    let cart = JSON.parse(localStorage.getItem('cart') || '[]');
+
+    let existing = cart.find((item: any) => item.id === product.id);
+
+    if (existing) {
+      existing.quantity += 1;
+    } else {
+      cart.push({
+        id: product.id,
+        title: product.title,
+        price: product.price,
+        thumbnail: product.thumbnail,
+        quantity: 1,
+      });
+    }
+
+    localStorage.setItem('cart', JSON.stringify(cart));
+  }
 }
